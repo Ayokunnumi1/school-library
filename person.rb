ebb1414fae17b8df644a3df583989baefa019642
Ayokunnumi1 <omololuayk@gmail.com>
2023-11-07 08:43:11 +0100

    Refactor codes in person.rb file

--- person.rb
+++ person.rb
@@ -3,19 +3,19 @@ class Person
   attr_reader :id
 
   def initialize(age, name: 'Unknown', parent_permission: true)
-    @id = nil
+    @id = SecureRandom.uuid
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
   def can_use_services?
-    true if of_age? || @parent_permission
+    if of_age? || @parent_permission
   end
 
   private
 
   def of_age?
-    true if age >= 18
+    if age >= 18
   end
 end
