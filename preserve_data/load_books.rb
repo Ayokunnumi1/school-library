60fbcdb6e494bbdfc0b91aeff6de47d669de086e
Ayokunnumi1 <omololuayk@gmail.com>
2023-11-16 16:43:12 +0100

    Create load_books.rb file & create code that reads the json array

--- preserve_data/load_books.rb
+++ preserve_data/load_books.rb
@@ -4,6 +4,14 @@ DATA_PATH = './data'.freeze
 Fileutils.mkdir_p(DATA_PATH)
 
 def load_books
+    file_path = "#{DATA_PATH}/books.json"
+
+    File.write(file_path, '[]') unless File.exist?(file_path)
+
+  json_data = File.read(file_path)
+  return if json_data.empty?
+
+  @books = JSON.parse(json_data).map { |data| Book.new(data['title'], data['author']) }
 
 end
 
