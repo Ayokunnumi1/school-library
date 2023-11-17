0a35142621a57489f1a7f5151fb80eb0c7b8d057
Ayokunnumi1 <omololuayk@gmail.com>
2023-11-17 14:41:12 +0100

    update require_relative path

--- student.rb
+++ student.rb
@@ -1,10 +1,10 @@
-require './person'
+require_relative 'person'
 
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
-    super(age, name: name, parent_permission: parent_permission)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
     classroom&.add_student(self)
   end
@@ -16,9 +16,5 @@ class Student < Person
   def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
-  end
-
-  def assign_id(id)
-    @id = id
   end
 end
